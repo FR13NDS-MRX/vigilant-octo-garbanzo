cfd71364b1f64c928c017470d795d049ed62b80b
Kirill Afanasyev <kirill_afan@mail.ru>
2022-02-17 21:19:09 +0700

    Добавьте файлы проекта.

---
+++ lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lab1
+{
+
+    public class HW1
+    {
+        public static long QueueTime(int[] customers, int n)
+        {
+            int cashboxCount = n;
+
+            if (customers.Length < cashboxCount)
+            {
+                cashboxCount = customers.Length;
+            }
+
+            int[] threads = new int[cashboxCount];
+
+            for (int j = 0; j < cashboxCount; j++)
+            {
+                threads[j] = customers[j];
+            }
+
+            return 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
