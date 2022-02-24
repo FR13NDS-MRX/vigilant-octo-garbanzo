2deff2c3602996b5099cbfa3aed3a174b88ac20c
Kirill Afanasyev <kirill_afan@mail.ru>
2022-02-24 17:01:57 +0700

    made it to working condition

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -21,7 +21,48 @@ namespace Lab1
                 threads[j] = customers[j];
             }
 
-            return 0;
+            int totalTime = 0;
+            int i = cashboxCount;
+
+            while (i < customers.Length)
+            {
+                int min = threads[0];
+
+                for (int j = 1; j < cashboxCount; j++)
+                {
+                    if (threads[j] < min)
+                    {
+                        min = threads[j];
+                    }
+                }
+
+                for (int j = 0; j < cashboxCount; j++)
+                {
+                    threads[j] -= min;
+                }
+                totalTime += min;
+
+                for (int j = 0; j < cashboxCount; j++)
+                {
+                    if (threads[j] == 0 && i < customers.Length)
+                    {
+                        threads[j] = customers[i];
+                        i++;
+                    }
+                }
+            }
+
+            int max = 0;
+
+            for (int j = 0; j < cashboxCount; j++)
+            {
+                if (threads[j] > max)
+                {
+                    max = threads[j];
+                }
+            }
+
+            return totalTime + max;
         }
     }
 
@@ -29,6 +70,20 @@ namespace Lab1
     {
         static void Main(string[] args)
         {
+            int[] q1 = { 5, 3, 4 };
+            Console.WriteLine(HW1.QueueTime(q1, 1));
+            int[] q2 = { 10, 2, 3, 3 };
+            Console.WriteLine(HW1.QueueTime(q2, 2));
+            int[] q3 = { 2, 3, 10 };
+            Console.WriteLine(HW1.QueueTime(q3, 2));
+            int[] q4 = { 533, 43, 10 };
+            Console.WriteLine(HW1.QueueTime(q4, 4));
+            int[] q5 = { 55 };
+            Console.WriteLine(HW1.QueueTime(q5, 1));
+            int[] q6 = { };
+            Console.WriteLine(HW1.QueueTime(q6, 3));
+            int[] q7 = { 5, 5, 5, 2 };
+            Console.WriteLine(HW1.QueueTime(q7, 3));
         }
     }
 }
